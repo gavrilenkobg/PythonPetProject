b8e1a9d104653cca4ae60a3011bc3e3fe51babe7
bohdan <gavrilenkobg@gmail.com>
2020-11-20 10:54:55 +0200

    added packeges to venv (requests, sql, selenium)

--- main.py
+++ main.py
@@ -39,6 +39,18 @@ def execute_query(connection, query):
         print(f"The error '{e}' occurred")
 
 
+# Function for run Select Queries
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
 create_users_table = """
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT, 
@@ -61,12 +73,37 @@ CREATE TABLE IF NOT EXISTS posts (
 ) ENGINE = InnoDB
 """
 
+create_likes_table = """
+CREATE TABLE IF NOT EXISTS likes (
+  id INT AUTO_INCREMENT, 
+  user_id INT NOT NULL, 
+  post_id INT NOT NULL, 
+  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id),
+  FOREIGN KEY fk_user_id(post_id) REFERENCES posts(id)
+);
+"""
 
-
+create_users = """
+INSERT INTO
+  `users` (`name`, `age`, `gender`, `nationality`)
+VALUES
+  ('James', 25, 'male', 'USA'),
+  ('Leila', 32, 'female', 'France'),
+  ('Brigitte', 35, 'female', 'England'),
+  ('Mike', 40, 'male', 'Denmark'),
+  ('Elizabeth', 21, 'female', 'Canada');
+"""
 
 connection = create_connection("localhost", "root", "Splinter222", "sm_app")
-execute_query(connection, create_users_table)
-execute_query(connection, create_posts_table)
+select_users = "SELECT * FROM users"
+users = execute_read_query(connection, select_users)
+for user in users:
+    print(user)
 
+
+
+# execute_query(connection, create_likes_table)
+# execute_query(connection, create_users_table)
+# execute_query(connection, create_posts_table)
 # create_database_query = "CREATE DATABASE sm_app"
 # create_database(connection, create_database_query)
